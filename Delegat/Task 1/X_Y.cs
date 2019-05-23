f54c91b1110a068b3a9beaae2454a7c5b8a2f4bc
Vladyslav <vladistchenko@gmail.com>
2019-05-23 18:48:50 +0300

    Create X_Y.cs

---
+++ Delegat/Task 1/X_Y.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    public delegate void XYZ(int a, int b);
+
+   public class X_Y
+    {
+        
+        public void Add(int a, int b)
+        {           
+            a = a * a;
+            b = b * b;
+            WOW?.Invoke(a,b);
+        }
+
+        public event XYZ WOW;
+    }
+}
