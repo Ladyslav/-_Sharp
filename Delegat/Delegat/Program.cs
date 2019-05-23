30630a4e06133fb3fa8d4fee84830f97545fa8fd
Vladyslav <vladistchenko@gmail.com>
2019-05-23 18:48:08 +0300

    Create Program.cs

---
+++ Delegat/Delegat/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var counter = new Counter();
+            counter.HundredReached += HandleHundreaached;
+            counter.Count();
+
+        }
+
+        public static void HandleHundreaached()
+        {
+            System.Console.WriteLine("HundredReached");
+            System.Console.WriteLine("Press Any KEY");
+            System.Console.ReadLine();
+        }
+    }
+}
