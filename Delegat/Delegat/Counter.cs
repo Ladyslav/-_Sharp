40cf1ca0a6f55bcccfb95d8de5ea535ce36efe58
Vladyslav <vladistchenko@gmail.com>
2019-05-23 18:47:38 +0300

    Create Counter.cs

---
+++ Delegat/Delegat/Counter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegat
+{
+
+  public delegate void HandHundredDelegate();
+
+
+
+  public class Counter
+    {
+        public void Count()
+        {
+            for (int i = 0; i<=100; i++)
+            {
+                if (i == 100)
+                {
+                    HundredReached?.Invoke();
+                    i = 0;
+                }
+            }
+        }
+        public event HandHundredDelegate HundredReached;
+    }
+}
