3e9f29fec673d3a0c4404816231b52acb31af6bc
Vladyslav <vladistchenko@gmail.com>
2019-05-23 18:50:01 +0300

    Create InputPlayer.cs

---
+++ neural networks/neural networks/InputPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace neural_networks
+{
+  class InputLayer
+    {
+          private (double[], double[])[] _trainset = new(double[], double[])[]//да-да, массив кортежей из 2 массивов
+        {
+            (new double[]{ 0, 0 }, new double[]{ 0, 1 }),
+            (new double[]{ 0, 1 }, new double[]{ 1, 0 }),
+            (new double[]{ 1, 0 }, new double[]{ 1, 0 }),
+            (new double[]{ 1, 1 }, new double[]{ 0, 1 })
+        };
+        
+        public (double[], double[])[] Trainset { get => _trainset; }//такие няшные свойства нынче в C# 7
+     
+    }
+}
