dd425025e97d773702de32f71c2b56cc6fdbf288
Vladyslav <vladistchenko@gmail.com>
2019-05-23 18:48:16 +0300

    Create B.cs

---
+++ Delegat/Task 1/B.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+  public class B
+    {
+        public void Show(int a, int b)
+        {
+            System.Console.WriteLine(a);
+            System.Console.WriteLine(b);
+        }
+    }
+}
