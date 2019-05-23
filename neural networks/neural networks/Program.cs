6ff15acfc36f56711602f16fbb5b900d7eff953d
Vladyslav <vladistchenko@gmail.com>
2019-05-23 18:51:50 +0300

    Create Program.cs

---
+++ neural networks/neural networks/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using static System.Math;
+using static System.Console;
+
+
+enum MemoryMode { GET, SET }
+enum NeuronType { Hidden, Output }
+
+namespace neural_networks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Network net = new Network();
+            Train(net);
+            Test(net);
+            ReadKey();
+        }
+    }
+}
