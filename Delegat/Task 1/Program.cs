eab00897fba8870c4d9a71324058b66b56f3cee9
Vladyslav <vladistchenko@gmail.com>
2019-05-23 18:48:38 +0300

    Create Program.cs

---
+++ Delegat/Task 1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+  public class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = new X_Y();
+            var tmp = new B();
+            result.WOW += tmp.Show;
+            result.Add(2, 3);
+            tmp.Show(5, 5);
+            System.Console.ReadKey();
+        }        
+    }
+}
