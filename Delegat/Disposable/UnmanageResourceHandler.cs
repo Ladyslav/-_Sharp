d17b56e5ecb3d20e31d60ef4b96d0e73037ebecb
Vladyslav <vladistchenko@gmail.com>
2019-05-26 18:24:57 +0300

    Create UnmanageResourceHandler.cs

---
+++ Delegat/Disposable/UnmanageResourceHandler.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Disposable
+{
+    class UnmanageResourceHandler : IDisposable
+    {
+        private Timer _timer;
+            public UnmanageResourceHandler()
+        {
+            _timer = new Timer(o => DoOnTick(o), null, 100, 100);
+        }
+
+        public void Start()
+        {
+            if (disposedValue == true)
+                throw new ObjectDisposedException("UnmanageResourceHandler");
+        }
+
+        private void DoOnTick(object obj)
+        {
+           
+            System.Console.WriteLine("Tick");
+            System.Console.ReadLine();
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _timer.Dispose();
+                    _timer = null;
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+        ~UnmanageResourceHandler()
+        {
+            Dispose(false);
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~UnmanageResourceHandler() {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+             GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
