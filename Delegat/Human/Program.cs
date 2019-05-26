1b632364f08e95fe42499376f9fa71b20f91c3b8
Vladyslav <vladistchenko@gmail.com>
2019-05-26 18:25:53 +0300

    Create Program.cs

---
+++ Delegat/Human/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Human
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var person = new Person("10", "Vlad", "Istchenko");
+            var _filePAth = @"D:\Person.txt";
+            using (var stream = new StreamWriter(_filePAth))
+            {
+                stream.WriteLine(person.ToString());
+            }
+        }
+    }
+}
