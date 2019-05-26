5e5842dd368ca9d5695a4eae695f37f36f0df50b
Vladyslav <vladistchenko@gmail.com>
2019-05-26 18:24:43 +0300

    Create FileExample.cs

---
+++ Delegat/Disposable/FileExample.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Disposable
+{
+   public class FileExample
+    {
+        private string _filePath = ;
+        public void WriteToFile(string message)
+        {
+            using(var file = File.Open(_file)
+        }
+    }
+}
