1fd0211b26bac2cd7c4fe3df0083ff35e35aced0
Vladyslav <vladistchenko@gmail.com>
2019-05-26 18:25:57 +0300

    Create Class1.cs

---
+++ Delegat/Lesson4/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4
+{
+  public  class Class1
+    {
+        internal void SaveFile(Func<string, IFile> download, string path)
+        {
+            var file = download(path);
+        }
+        public delegate void EvenHandler(object sender, EventArgs args);
+    }
+}
