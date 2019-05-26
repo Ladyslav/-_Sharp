fc3e34c10f66cc588ba71b23260d32eabe5db442
Vladyslav <vladistchenko@gmail.com>
2019-05-26 18:26:09 +0300

    Create ResourceUser.cs

---
+++ Delegat/Disposable/ResourceUser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disposable
+{
+   internal class ResourceUser
+    {
+        internal void Do()
+        {
+            var resource = new UnmanageResourceHandler();
+            using (resource)
+            {
+                resource.Start();
+            }
+
+            var anotherResource = new UnmanageResourceHandler();
+            try
+            {
+                resource.Start();
+            }
+            finally
+            {
+                resource.Dispose();
+            }
+        }
+
+        internal void DoSomeThingVisibleOblyInternal()
+        {
+
+        }
+    }
+}
