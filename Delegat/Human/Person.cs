779047d2623a6e6f9357309ce6266768d988edea
Vladyslav <vladistchenko@gmail.com>
2019-05-26 18:25:47 +0300

    Create Person.cs

---
+++ Delegat/Human/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Human
+{
+    public class Person
+    {
+       public string Id { get; }
+       public string Name { get; }
+       public string Surname { get; }
+
+
+        public Person(string _id, string _name, string _surname)
+        {
+            Id = _id;
+            Name = _name;
+            Surname = _surname;
+        }
+
+        public override string ToString()
+        {
+            return $"ID={Id} Name={Name} Surname={Surname}";
+        }
+        
+    }
+
+}
