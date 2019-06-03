ce2d69ebbdbbe57557401dec9beac21de1f7730f
Vladyslav <vladistchenko@gmail.com>
2019-06-03 21:00:10 +0300

    Update Program.cs

--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,23 @@ namespace Server
     {
         static void Main(string[] args)
         {
+            try
+            {
+                TcpListener serverSocket = new TcpListener(IPAddress.Any, 7000);
+                Console.WriteLine("Server started");
+                serverSocket.Start();
+                TcpClient clientSocket = serverSocket.AcceptTcpClient();
+
+
+                clientSocket.Close();
+                serverSocket.Stop();
+                Console.WriteLine("Server stopped");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
         }
     }
 }
