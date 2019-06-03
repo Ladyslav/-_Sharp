91e264ea14447253234087b94ece25a2a3a8bc46
Vladyslav <vladistchenko@gmail.com>
2019-06-03 22:09:05 +0300

    Update Program.cs

--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -17,13 +17,34 @@ namespace Server
                 TcpListener serverSocket = new TcpListener(IPAddress.Any, 7000);
                 Console.WriteLine("Server started");
                 serverSocket.Start();
-                TcpClient clientSocket = serverSocket.AcceptTcpClient();
 
 
-                clientSocket.Close();
+                while (true)
+                {
+                    TcpClient clientSocket = serverSocket.AcceptTcpClient();
+                    NetworkStream stream = clientSocket.GetStream();
+
+                    byte[] bytes = new byte[256];
+                   int lenght = stream.Read(bytes, 0, bytes.Length);
+                    string request = Encoding.ASCII.GetString(bytes, 0, lenght);
+                    Console.WriteLine("Got request" + request);
+
+
+                    string message = "Length of your request:" + request.Length;
+                    bytes = Encoding.ASCII.GetBytes(message);
+                    stream.Write(bytes, 0, bytes.Length);
+                    stream.Flush();
+
+
+                    Console.WriteLine("Send Message: " + message);
+                    clientSocket.Close();
+                }
+
+
                 serverSocket.Stop();
                 Console.WriteLine("Server stopped");
             }
+
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
