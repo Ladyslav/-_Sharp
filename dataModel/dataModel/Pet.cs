a01a7dd1da21c5fa4353566eee056702b92003d8
Vladyslav <vladistchenko@gmail.com>
2019-06-07 14:39:39 +0300

    Create Pet.cs

---
+++ dataModel/dataModel/Pet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dataModel
+{
+  public  class Pet
+    {
+        public Child Owner { get; set; }
+    }
+}
