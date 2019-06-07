833231d5ac65c5a60c6981af825160b56fabd320
Vladyslav <vladistchenko@gmail.com>
2019-06-07 15:05:44 +0300

    Create Program.cs

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Client
+{
+    class Program
+    {
+        static Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        static MemoryStream stream = new MemoryStream(buffer);
+        static BinaryWriter writer = new BinaryWriter(stream);
+        static byte[] buffer = new byte[1024];
+
+        static void Main(string[] args)
+        {
+            Title = "Client";
+            socket.Connect("127.0.0.1", 904);
+            buffer = Encoding.ASCII.GetBytes("password" + "@" + "login");
+
+            writer.Write("login");
+            writer.Write("password");
+
+            socket.Send(buffer);
+
+            ReadLine();
+
+        }
+    }
+}
