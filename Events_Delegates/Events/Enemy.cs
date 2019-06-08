75bc6a9ebad719d08c297735029c4d810ebdfd51
Vladyslav <vladistchenko@gmail.com>
2019-06-08 16:48:04 +0300

    Create Enemy.cs

---
+++ Events_Delegates/Events/Enemy.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    class Enemy
+    {        
+        private int _health = 100;
+        public event DeadHandler OnEnemyDead;
+        public bool isDead => _health <= 0;
+
+        public Enemy(DeadHandler onEnemyDead, int heath = 100)
+        {
+            OnEnemyDead = onEnemyDead;
+        }
+
+        public void Attack(int damage)
+        {
+            if (!isDead)
+            {
+                _health -= damage;
+                if (isDead)
+                {
+                    OnEnemyDead?.Invoke(this);
+                }
+            }
+        }
+
+        public void Update()
+        {
+            Attack(1);
+        }
+    }
+}
