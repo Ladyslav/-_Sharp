56e11c3dcbfc1b236ce7c02a60fb9d82c24b7140
Vladyslav <vladistchenko@gmail.com>
2019-06-08 16:47:40 +0300

    Update Program.cs

--- Events_Delegates/Events_Delegates/Program.cs
+++ Events_Delegates/Events_Delegates/Program.cs
@@ -8,8 +8,36 @@ namespace Events_Delegates
 {
     class Program
     {
+        delegate bool Comparer(string a, string b);
+ 
         static void Main(string[] args)
         {
+            string[] array = { "T", "TE", "Tst", "Teeets" };
+            Sort(array, new LengthComparer());
+
+            foreach(var e in array)
+            {
+                Console.WriteLine(e);
+            }
+            Console.ReadKey();
+        }
+
+        
+        
+        static void Sort(string[] array, IStringComparer comparer)
+        {
+            for(int p=0; p<=array.Length - 2; p++)
+            {
+                for(int i=0; i <= array.Length - 2; i++)
+                {
+                    if (comparer.Compare(array[i], array[i + 1]))
+                    {
+                        string t = array[i + 1];
+                        array[i + 1] = array[i];
+                        array[i] = t;
+                    }
+                }
+            }
         }
     }
 }
