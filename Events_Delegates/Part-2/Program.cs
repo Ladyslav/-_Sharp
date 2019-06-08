0c48ff1edf5a756f558203dc795b79729bf8fbe1
Vladyslav <vladistchenko@gmail.com>
2019-06-08 16:47:51 +0300

    Create Program.cs

---
+++ Events_Delegates/Part-2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_2
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Validator emailValidator = new Validator(new Validate[] {MaxChar, IsEmail});
+            string input = Console.ReadLine();
+            if (emailValidator.IsValid(input))
+            {
+                Console.WriteLine("Message was sent!");
+            }
+            else
+            {
+                Console.WriteLine("Sorry, I'm wasted");
+            }
+            Console.ReadKey();
+        }
+
+        static bool MaxChar(string s)
+        {
+            return s.Length > 0 && s.Length < 255;
+        }
+
+        static bool IsEmail(string s)
+        {
+            return s.Contains("@");
+        }
+
+    }
+}
