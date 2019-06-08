46f2fa5959f6bb61fbdc5fb937a6ba6a6e87cb58
Vladyslav <vladistchenko@gmail.com>
2019-06-08 16:48:10 +0300

    Create Player.cs

---
+++ Events_Delegates/Events/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    class Player
+    {
+        public void AttackEnemys(Enemy[] enemys)
+        {
+            Enemy e = enemys[Program.R.Next(0, enemys.Length)];
+            e.Attack(10);
+        }
+    }
+}
