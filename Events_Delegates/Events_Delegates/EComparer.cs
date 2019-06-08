ce073a5b995bfce9f0f258b120998d7f5a68cf9f
Vladyslav <vladistchenko@gmail.com>
2019-06-08 16:47:56 +0300

    Create EComparer.cs

---
+++ Events_Delegates/Events_Delegates/EComparer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events_Delegates
+{
+    class EComparer : IStringComparer
+    {
+        public bool Compare(string a, string b)
+        {
+            int eA = 0, eB = 0;
+            int maxCount = a.Length > b.Length ? a.Length : b.Length;
+            for (int i = 0; i < maxCount; i++)
+            {
+                if (a.Length > i)
+                {
+                    if (a[i] == 'e')
+                    {
+                        eA++;
+                    }
+                }
+                if (b.Length > i)
+                {
+                    if (b[i] == 'e')
+                    {
+                        eB++;
+                    }
+                }
+            }
+            return eA < eB;
+        }
+    }
+}
