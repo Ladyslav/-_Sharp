8bbf61501b0ac31b28b176f8871fd8302b02a65b
Vladyslav <vladistchenko@gmail.com>
2019-06-08 16:47:53 +0300

    Create Validator.cs

---
+++ Events_Delegates/Part-2/Validator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_2
+{
+    delegate bool Validate(string s);
+
+
+    class Validator
+    {
+        public Validate[] _validators;
+
+        public Validator(Validate[] validators)
+        {
+            _validators = validators;
+        }
+
+        public bool IsValid(string s)
+        {
+            bool isValid = true;
+            foreach(var validator in _validators)
+            {
+                if (!validator(s))
+                {
+                    isValid = false;
+                    break;
+                }
+            }
+            return isValid;
+        }
+    }
+}
