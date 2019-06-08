4f51aa0887de86316ff82c2b17b3a56a56c8d1f7
Vladyslav <vladistchenko@gmail.com>
2019-06-08 13:49:35 +0300

    Create Program.cs

---
+++ ConsoleApp1/Server/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Server
+{
+    class Program
+    {
+        static Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        static byte[] buffer = new byte[1024];
+        static MemoryStream stream = new MemoryStream(buffer);
+        static BinaryReader reader = new BinaryReader(stream);
+    
+
+        static void Main(string[] args)
+        {
+            Title = "Server";
+            socket.Bind(new IPEndPoint(IPAddress.Any, 904));
+            socket.Listen(5);
+
+            Socket client = socket.Accept();
+            client.Receive(buffer);
+
+            string login = reader.ReadString();
+            string password = reader.ReadString();
+
+            stream.Position = 0;
+
+            WriteLine(login);
+            WriteLine(password);
+
+
+            ReadLine();
+        }
+    }
+}
