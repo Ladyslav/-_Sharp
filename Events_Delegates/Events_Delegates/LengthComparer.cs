0433b68252ab4f4d042abb7df6f41e3a9318452a
Vladyslav <vladistchenko@gmail.com>
2019-06-08 16:47:43 +0300

    Create LengthComparer.cs

---
+++ Events_Delegates/Events_Delegates/LengthComparer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events_Delegates
+{
+    class LengthComparer : IStringComparer
+    {
+        public bool Compare(string a, string b)
+        {
+            return a.Length > b.Length;
+        }
+    }
+}
