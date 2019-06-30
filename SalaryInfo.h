2ba3cbb544cf0031b23d7b8afa7021574c562989
Vladyslav <vladistchenko@gmail.com>
2019-07-01 02:19:52 +0300

    Create SalaryInfo.h

---
+++ SalaryInfo.h
@@ -0,0 +1,10 @@
+#pragma once
+
+struct SalaryInfo
+{
+	unsigned int Bonus = 0;
+	unsigned int LessonDuration = 0;
+	unsigned int LessonBase = 0;
+	unsigned int MGREficienty = 0;
+	unsigned int HREficienty = 0;
+};
